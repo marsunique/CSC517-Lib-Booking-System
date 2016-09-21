7f0cdffeded48bdf6f683d5e530de8924d1690ba
ddyvas@gmail.com <ddyvas@gmail.com>
2016-09-21 01:01:30 -0400

    NeedToAuthenticate

--- CSC517-Lib-Booking-System/db/schema.rb
+++ CSC517-Lib-Booking-System/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921031024) do
+ActiveRecord::Schema.define(version: 20160921032835) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
     t.string   "name"
     t.string   "authority"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
