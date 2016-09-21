f0c1f20bb13dd83d61ab666826fe5cf092d562cc
ddyvas@gmail.com <ddyvas@gmail.com>
2016-09-21 11:47:08 -0400

    finish login model without judge authority

--- CSC517-Lib-Booking-System/app/models/user.rb
+++ CSC517-Lib-Booking-System/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   #attr_accessor :email, :password, :name ,:authority
   before_save { self.email = email.downcase }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 
 end
