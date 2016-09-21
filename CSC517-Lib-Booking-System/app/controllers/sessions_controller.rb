16266b5340c623316831d63cf03ca178de5766f4
ddyvas@gmail.com <ddyvas@gmail.com>
2016-09-21 14:28:45 -0400

    implement wrong password alert and log out

--- CSC517-Lib-Booking-System/app/controllers/sessions_controller.rb
+++ CSC517-Lib-Booking-System/app/controllers/sessions_controller.rb
@@ -8,11 +8,13 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render "new"
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- CSC517-Lib-Booking-System/config/routes.rb
+++ CSC517-Lib-Booking-System/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  root 'sessions#new'
   get 'static_pages/about'
 
   get 'static_pages/contact'
