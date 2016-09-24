6ef801d2c7dc2cd98489b44c876c6a80089e0c61
ddyvas@gmail.com <ddyvas@gmail.com>
2016-09-24 17:53:56 -0400

    finish search room except by building

--- CSC517-Lib-Booking-System/app/models/room.rb
+++ CSC517-Lib-Booking-System/app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
   def self.search(search)
     where("number LIKE ?", "%#{search}%")
+    where("size LIKE ?", "%#{search}%")
+    #where("building LIKE ?", "%#{search}%")
   end
 end
