39fac84ea85644e9510fcceabafdd2de0ed58ee3
ddyvas@gmail.com <ddyvas@gmail.com>
2016-09-26 10:52:50 -0400

    make superadmin login

--- CSC517-Lib-Booking-System/app/controllers/sessions_controller.rb
+++ CSC517-Lib-Booking-System/app/controllers/sessions_controller.rb
@@ -3,22 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase, authority: '1')
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user #need to change as manager
-
-    else
-      user = User.find_by(email: params[:session][:email].downcase, authority: '0')
-      if user && user.authenticate(params[:session][:password])
+      if(user.authority == '0')
         log_in user
         redirect_to root_url
+      elsif(user.authority == '1' || user.authority == '2')
+          log_in user
+          redirect_to user #need to change as manager
       else
+        flash.now[:danger] = 'Invalid message, please contact admin'
+        render "new"
+      end
+    else
       flash.now[:danger] = 'Invalid email/password combination'
       render "new"
-      end
-
     end
+
   end
 
   def welcome
